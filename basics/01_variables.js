4f040d425a700de334150ca82f88af07310428b0
bhuvaarchit <138206745+bhuvaarchit@users.noreply.github.com>
2024-11-10 21:23:15 +0530

    archit10-11

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountid = 1122883;
+let accountemail = "archit@gmail.com";
+var accountpassword = "12345";
+accountcity = "surat"
+
+// accountid = 2; // Not allowed
+
+console.log(accountid);
